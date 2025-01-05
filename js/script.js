d93236ef7f5f9035f4c03b60a3fca07bc3e00abe
Christoffer <christoffer.isenberg@gmail.com>
2025-01-05 14:12:03 +0100

    working but ugly

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 
 $(document).ready(function () {
     const filePath = 'js/words.txt';
@@ -7,14 +8,105 @@ $(document).ready(function () {
     const elapsedTimeDisplay = $("#elapsedTime");
     const wordCountDisplay = $("#wordCount");
     const wordsPerMinuteDisplay = $("#wordsPerMinuteDisplay");
+    const wrongCharacterDisplay = $("#wrongCharacterDisplay");
 
-    let words = [];
-    let currentWordIndex = 0;
-    let CurrentCharacterIndex = 0;
     let started = false;
+    let isComplete = false;
     let incorrectTimeout = null;
     let timer = null;
     let startTime = null;
+    let wrongCharacters = 0;
+    
+    const maxCharactersPerRow = 64;
+    const rows = [];
+
+    let currentWordIndex = 0;
+    let currentCharacterIndex = 0;
+    let currentRowIndex = 0;
+
+
+
+    function initializeRows(words) {
+        rows.length = 0;
+        let currentRow = { index: 0, words: [], characterCount: 0 };
+
+        words.forEach(word => {
+            // Get added space if there is a word infront of it
+            const addedSpace = (currentRow.words.length > 0 ? 1 : 0)
+            const wordLengthWithSpace = word.length + addedSpace;
+
+            if (currentRow.characterCount + wordLengthWithSpace <= maxCharactersPerRow) {
+                currentRow.words.push(word);
+                currentRow.characterCount += wordLengthWithSpace;
+            } else {
+                rows.push(currentRow)
+                currentRow = { index: rows.length, words: [word], characterCount: word.length }
+            }
+        });
+
+        if (currentRow.words.length > 0) {
+            rows.push(currentRow)
+        }
+    }
+
+    function generateRowHtml(row) {
+        
+        const rowHtml = row.words.map((word, wordIndex) => {
+
+            const absoluteWordIndex = calculateAbsoluteWordIndex(row.index, wordIndex);
+            
+            // completed
+            if(row.index < currentRowIndex || absoluteWordIndex < currentWordIndex) {
+                return generateWordHtml(word, false, 0);
+            } 
+            // current
+            else if (row.index === currentRowIndex && absoluteWordIndex === currentWordIndex) {
+                return generateWordHtml(word, true, currentCharacterIndex)
+            } 
+            // future
+            else {
+                return `<span class="future">${word}</span>`;
+            }
+        }).join(" ")
+
+        return `<p class="row">${rowHtml}</p>`;
+    }
+
+    function calculateAbsoluteWordIndex(rowIndex, wordIndex) {
+        const totalWordsInPreviousRows = rows.slice(0, rowIndex)
+            .reduce((sum, row) => sum + row.words.length, 0);
+        
+            return totalWordsInPreviousRows + wordIndex;
+    }
+
+    function generateWordHtml(word, isCurrentWord, currentCharIndex) {
+        // if not current word
+        if (!isCurrentWord) {
+            return `<span class="correct">${word}</span>`
+        }
+
+        // split into characters and deal with highlights on character basis then join back together
+        return word.split("")
+                .map((char, charIndex) => {
+                    if (charIndex < currentCharIndex) {
+                        return `<span class="correct">${char}</span>`;
+                    } else if (charIndex === currentCharIndex) {
+                        return `<span class="highlight">${char}</span>`;
+                    } else {
+                        return `<span class="future">${char}</span>`;
+                    }
+                }).join("");
+    }
+
+    function renderText() {
+        rows.forEach((row) => {
+            row.renderedHtml = generateRowHtml(row);
+        })
+
+        const html = rows.map((row) => row.renderedHtml).join("");
+
+        typingBox.html(html);
+    }
 
     /*  Fisher-Yates Shuffle */
     const shuffleArray = (array) => {
@@ -22,7 +114,6 @@ $(document).ready(function () {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
-
         return array;
     };
 
@@ -38,7 +129,7 @@ $(document).ready(function () {
                 .then(data => {
 
                     const words = data.split("\r\n");
-                    const randomWords = shuffleArray(words).slice(0, 200);
+                    const randomWords = shuffleArray(words).slice(0, 15);
                     resolve(randomWords);
                 })
                 .catch(error => {
@@ -48,42 +139,21 @@ $(document).ready(function () {
         });
     };
 
-    const renderText = () => {
-        const html = words.map((word, wordIndex) => {
-            // Already completed
-            if (wordIndex < currentWordIndex) {
-                return `<span class="correct">${word}</span>`;
-            }
-            // Current Word
-            if (wordIndex === currentWordIndex) {
-                const chars = word.split("").map((char, charIndex) => {
-                    if (charIndex < CurrentCharacterIndex) {
-                        return `<span class="correct">${char}</span>`;
-                    }
-                    if (charIndex === CurrentCharacterIndex) {
-                        return `<span class="highlight">${char}</span>`;
-                    }
-                    return char;
-                });
-                return chars.join("");
-            }
-            return word;
-        }).join(" ");
-
-        typingBox.html(html);
-    }
-
     document.addEventListener('keypress', (event) => {
+        if (isComplete) {
+            return;
+        }
+
         const newText = event.key === " " ? "␣" : event.key;
         currentKeyDisplay.text(newText);
 
-        let currentWord = words[currentWordIndex];
-        let currentChar = currentWord[CurrentCharacterIndex];
+        let currentRow = rows[currentRowIndex];
+        let currentWord = currentRow.words[currentWordIndex - calculateAbsoluteWordIndex(currentRowIndex, 0)]
+        let currentChar = currentWord[currentCharacterIndex];
 
         if (event.key === " ") {
             return;
         }
-
 
         if (event.key === currentChar) {
             if (!started) {
@@ -94,21 +164,30 @@ $(document).ready(function () {
                 clearTimeout(incorrectTimeout);
                 incorrectTimeout = null;
             }
-            CurrentCharacterIndex++;
+            currentCharacterIndex++;
 
-            if (CurrentCharacterIndex === currentWord.length) {
+            if (currentCharacterIndex === currentWord.length) {
                 console.log("Completed word!")
-                CurrentCharacterIndex = 0;
+                currentCharacterIndex = 0;
                 currentWordIndex++;
             }
+            if (currentWordIndex === calculateAbsoluteWordIndex(currentRowIndex + 1, 0)) {
+                console.log("finished a row!!!")
+                currentRowIndex++;
 
-            if (currentWordIndex === words.length) {
-                // Finished!
+                if(currentRowIndex === rows.length) {
+                    stopTimer();
+                    isComplete = true;
+                    alert("Finished!")
+                }
             }
+
         }
         else {
             // Wrong character
             console.log(`Wrong character wanted ${currentChar} got ${event.key}`);
+
+            wrongCharacters++;
 
             // Update highlighted character to be red
             const wrongHtml = `<span class="incorrect">${currentChar}</span>`;
@@ -126,9 +205,10 @@ $(document).ready(function () {
         renderText();
     })
 
-    const startTimer = () => {
+    function startTimer() {
         started = true;
         startTime = new Date();
+
         timer = setInterval(() => {
 
             const wordCount = currentWordIndex;
@@ -144,20 +224,30 @@ $(document).ready(function () {
             const calculatedWpm = Math.round((wordCount / elapsedTime) * 60);
             const wpmDisplay = startTime ? calculatedWpm : 0;
             wordsPerMinuteDisplay.text(wpmDisplay)
-        }, 1000)
 
+            // Display typos
+            wrongCharacterDisplay.text(wrongCharacters);
+        }, 1000)
     }
 
-    // Actual doing stuff...  
+    function stopTimer() {
+        if(!startTimer) {
+            return;
+        }
+
+        clearInterval(timer);
+        timer = null;
+    }
+
     loadWords()
         .then(fetchedWords => {
-            words = fetchedWords;
+            //words = fetchedWords;
+            initializeRows(fetchedWords)
             renderText();
         })
         .catch(error => {
             console.error("Error loading words:", error);
-            // TODO Add some error message?
-        }
-    );
+            alert("Error loading words");
+        });
 })
 
